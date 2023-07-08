1c367eff8391d79e21998f47709ee608fb3eb923
Spline <splinestein@gmail.com>
2023-07-08 03:07:02 +0300

    Read user input.
    Reads basic user input for sign, exponent and mantissa in binary format.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                Converstion will happen later. 
+                Datatype has to be string because leading 0's would otherwise be ignored.
+                Adding length check and 0 || 1 only int verification later.
+            */
+
+            Console.WriteLine("(binary64) Set sign: 1 bit.");
+            string sign = Console.ReadLine();
+
+            Console.WriteLine("(binary64) Set exponent, 8 bits.");
+            string exponent = Console.ReadLine();
+
+            Console.WriteLine("(binary64) Set mantissa / fraction, 52 bits.");
+            string mantissa = Console.ReadLine();
+
+            Console.WriteLine($"{sign} {exponent} {mantissa}");
+        }
+    }
+}
